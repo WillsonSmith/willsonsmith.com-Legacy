1bdff956b4bcf37074b74df289865f12fa22fb71
Willson Smith <me@willsonsmith.com>
2020-02-11 22:42:34 -0500

    complete overhaul, start fresh with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addPassthroughCopy('src/js');
+  return {
+    dir: {
+      input: 'src',
+      includes: '_includes',
+      layouts: '_layouts',
+    },
+  };
+};
