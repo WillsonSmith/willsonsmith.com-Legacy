016d16ea60ac79fbedd361accc304713a1a64f95
Willson Smith <me@willsonsmith.com>
2022-04-24 15:28:22 -0400

    placeholder pop

--- assets/js/components/audio-elements/spatial-pop.js
+++ assets/js/components/audio-elements/spatial-pop.js
@@ -1,3 +1,4 @@
+import { Howl, Howler } from 'howler';
 import { html, css, LitElement } from 'lit';
 
 let demoEvents = ['click', 'hover', 'scroll', 'touchstart'];
@@ -13,6 +14,7 @@ class SpatialPop extends LitElement {
   static get properties() {
     return {
       events: { type: Array, attribute: 'events' },
+      src: { type: String, attribute: 'src' },
       playing: { type: Boolean, attribute: 'playing' },
     };
   }
@@ -25,13 +27,33 @@ class SpatialPop extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
+    const sources = this.querySelectorAll('pop-source');
+    for (const source of sources) {
+    }
   }
   disconnectedCallback() {
     super.disconnectedCallback();
   }
 
   updated(changedProperties) {
-    console.log(this.events);
+    if (changedProperties.has('src')) {
+      console.log('src changed');
+      console.log(this.src);
+      this.playing = false;
+      this.audio = new Howl({
+        src: this.src,
+        sprite: {
+          pop: [500, 1000],
+        },
+        onplay: () => {
+          this.playing = true;
+        },
+        onend: () => {
+          this.playing = false;
+        },
+      });
+    }
+
     if (changedProperties.has('events')) {
       const previousEvents = changedProperties.get('events') || [];
       const removedEvents = previousEvents.filter(
@@ -52,16 +74,23 @@ class SpatialPop extends LitElement {
   }
 
   render() {
-    return html` <slot></slot>`;
+    return html`<slot></slot>`;
   }
 
   _handleEvent(event) {
     const { target } = event;
     if (!this.contains(target)) return;
 
-    const x = (target.offsetLeft + target.offsetWidth / 2) / this.offsetWidth;
-    const y = (target.offsetTop + target.offsetHeight / 2) / this.offsetHeight;
+    // get the relative position of the centre of the target to the centre of the container
+    // get number between -1 and 1 for each axis
+    const x =
+      (target.offsetLeft + target.offsetWidth / 2) / this.offsetWidth - 1;
+    const y =
+      (target.offsetTop + target.offsetHeight / 2) / this.offsetHeight - 1;
     console.log(x, y);
+
+    this.audio.orientation(x, y, 0);
+    this.audio?.play('pop');
   }
 }
 
