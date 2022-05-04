7fd93200717d6a4ddea7f58b91f94b6e2976fa5b
Willson Smith <me@willsonsmith.com>
2022-05-04 19:59:53 -0400

    first thing

--- assets/js/shoelace.js
+++ assets/js/shoelace.js
@@ -1,5 +1,6 @@
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
 import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
+import '@shoelace-style/shoelace/dist/components/avatar/avatar.js';
 
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 setBasePath('/assets/vendor/shoelace/dist');
