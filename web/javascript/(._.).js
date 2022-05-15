1b2eb902cb6fcebdf1d30913c422415281c425b3
Willson Smith <me@willsonsmith.com>
2022-05-15 12:04:24 -0400

    update with new code

--- web/javascript/(._.).js
+++ web/javascript/(._.).js
@@ -4,6 +4,8 @@ setBasePath(`/vendor/modules/shoelace/dist`);
 
 /** Shoelace components used on index */
 import '@shoelace-style/shoelace/dist/components/card/card.js';
+import '@shoelace-style/shoelace/dist/components/avatar/avatar.js';
+import '@shoelace-style/shoelace/dist/components/divider/divider.js';
 
 /** Main page required components components */
 
