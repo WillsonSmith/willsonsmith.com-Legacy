4bd7a921f1ceeafe32b836be979a5cb15f46c3bf
Willson Smith <me@willsonsmith.com>
2022-06-18 17:11:42 -0400

    some neat stacking i guess

---
+++ web/javascript/components/stacking-scroller.js
@@ -0,0 +1,46 @@
+import {LitElement, html, css} from 'lit';
+
+class StackingScroller extends LitElement {
+  static get styles() {
+    return css`
+      .stackable {
+        position: relative;
+      }
+      .stackable::slotted(.stackable-element) {
+        position: fixed;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      offset: { type: Number },
+      stackOffset: { type: Number, attribute: `stack-offset` },
+    };
+  }
+
+  constructor() {
+    super();
+    this.offset = 0;
+    this.stackOffset = 20;
+  }
+
+  render() {
+    return html`
+      <div class="stackable">
+        <slot @slotchange=${this._slotChange}></slot>
+      </div>
+    `;
+  }
+
+  _slotChange() {
+    const stackableElements = Array.from(this.children);
+    console.log(stackableElements);
+    stackableElements.forEach((element, index) => {
+      element.style.position = `sticky`;
+      element.style.top = `${this.offset + index * this.stackOffset}px`;
+    });
+  }
+}
+
+customElements.define(`stacking-scroller`, StackingScroller);
