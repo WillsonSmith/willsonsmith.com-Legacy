ce28a497631425d177d1daf20eba5fdf7ddbafd3
Willson Smith <me@willsonsmith.com>
2022-09-05 02:18:14 -0400

    change how dark mode works

--- web/javascript/web-application.js
+++ web/javascript/web-application.js
@@ -31,14 +31,20 @@ class WebApplication extends LitElement {
     title: { type: String },
   };
 
+  firstUpdated() {
+    const darkMode = localStorage.getItem(`darkMode`);
+    this.darkMode = darkMode === `true`;
+  }
+
   updated(changedProperties) {
     if (changedProperties.has(`darkMode`)) {
       this.class = this.darkMode ? `sl-theme-dark` : ``;
     }
   }
 
-  handleThemeChange(event) {
-    this.darkMode = !event.target.checked;
+  handleThemeChange() {
+    this.darkMode = !this.darkMode;
+    localStorage.setItem(`darkMode`, this.darkMode.toString());
   }
 
   render() {
@@ -46,7 +52,10 @@ class WebApplication extends LitElement {
       <div class=${classMap({ "sl-theme-dark": this.darkMode })}>
         <div class="page-header">
           <h1>Home</h1>
-          <sl-switch checked @sl-change=${this.handleThemeChange}>
+          <sl-switch
+            .checked=${this.darkMode}
+            @sl-change=${this.handleThemeChange}
+          >
             <sl-icon name=${this.darkMode ? `moon` : `sun`}></sl-icon>
           </sl-switch>
         </div>
