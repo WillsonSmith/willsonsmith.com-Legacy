60a0ad30f1b3484a88ab57674d6a1d9fed6ee36c
Willson Smith <me@willsonsmith.com>
2022-09-09 16:11:38 -0400

    s

--- web/javascript/boot.js
+++ web/javascript/boot.js
@@ -1,10 +1,9 @@
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 setBasePath(`/vendor/modules/shoelace/dist`);
 
-import "@shoelace-style/shoelace/dist/components/card/card.js";
 import "@shoelace-style/shoelace/dist/components/avatar/avatar.js";
-import "@shoelace-style/shoelace/dist/components/divider/divider.js";
 
+import "../../shared/web-components/yz-block-card/yz-block-card.js";
 import "../../shared/web-components/page-header/page-header.js";
 import "../../shared/web-components/colorize-word/colorize-word.js";
 
