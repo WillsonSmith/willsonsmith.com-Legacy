bf0e646cba00e4784650922b0823985b22345a95
Willson Smith <me@willsonsmith.com>
2023-04-10 01:09:07 -0400

    small change to reduce index size

---
+++ src/index.css.ts
@@ -0,0 +1,84 @@
+import { css } from 'lit';
+export const styles = css`
+  @import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
+  body {
+    line-height: var(--line-height-small);
+  }
+  site-header {
+    display: block;
+    padding-inline: var(--spacing-lg);
+    padding-block: var(--spacing);
+    position: sticky;
+    width: 100%;
+    top: 0;
+
+    background: linear-gradient(
+      to bottom,
+      rgba(255, 255, 255, 0.3) 30%,
+      rgba(255, 255, 255, 0) 100%
+    );
+
+    -webkit-backdrop-filter: blur(1px);
+    backdrop-filter: blur(1px);
+  }
+
+  site-header::part(heading) {
+    font-family: 'Lilita One', var(--font-system-sans);
+  }
+
+  .section {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-sm);
+    align-items: center;
+    padding: var(--spacing);
+  }
+
+  .section header h2 {
+    font-family: 'Lilita One', var(--font-system-sans);
+    font-weight: var(--font-weight-bold);
+    font-size: var(--font-size-xxl);
+    line-height: var(--line-height-xs);
+  }
+
+  .page {
+    margin-block-start: var(--spacing-lg);
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-lg);
+  }
+
+  .bio {
+    display: flex;
+    flex-direction: column;
+
+    line-height: var(--line-height-sm);
+  }
+
+  .bio > * + * {
+    margin-block-start: var(--spacing-sm);
+  }
+
+  a {
+    color: var(--color-text-body);
+  }
+
+  .project-list {
+    background: var(--sl-color-neutral-100);
+    padding: var(--spacing-sm);
+    line-height: var(--line-height);
+  }
+
+  .project-list__heading {
+    line-height: var(--line-height);
+    font-weight: var(--font-weight-bold);
+    font-size: var(--font-size-lg);
+  }
+
+  ul[role='list'].project-list__list {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-xs);
+    line-height: var(--line-height-sm);
+  }
+`;
--- src/index.pyre.ts
+++ src/index.pyre.ts
@@ -1,4 +1,4 @@
-import { css, html, isServer } from 'lit';
+import { html, isServer } from 'lit';
 
 export const title = "Willson's fun times website place";
 export const description = 'Willson is a front-end developer who likes to make fun things.';
@@ -10,89 +10,7 @@ export const links = [
   },
 ];
 
-export const styles = css`
-  @import url('https://fonts.googleapis.com/css2?family=Lilita+One&display=swap');
-  body {
-    line-height: var(--line-height-small);
-  }
-  site-header {
-    display: block;
-    padding-inline: var(--spacing-lg);
-    padding-block: var(--spacing);
-    position: sticky;
-    width: 100%;
-    top: 0;
-
-    background: linear-gradient(
-      to bottom,
-      rgba(255, 255, 255, 0.3) 30%,
-      rgba(255, 255, 255, 0) 100%
-    );
-
-    -webkit-backdrop-filter: blur(1px);
-    backdrop-filter: blur(1px);
-  }
-
-  site-header::part(heading) {
-    font-family: 'Lilita One', var(--font-system-sans);
-  }
-
-  .section {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-sm);
-    align-items: center;
-    padding: var(--spacing);
-  }
-
-  .section header h2 {
-    font-family: 'Lilita One', var(--font-system-sans);
-    font-weight: var(--font-weight-bold);
-    font-size: var(--font-size-xxl);
-    line-height: var(--line-height-xs);
-  }
-
-  .page {
-    margin-block-start: var(--spacing-lg);
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-lg);
-  }
-
-  .bio {
-    display: flex;
-    flex-direction: column;
-
-    line-height: var(--line-height-sm);
-  }
-
-  .bio > * + * {
-    margin-block-start: var(--spacing-sm);
-  }
-
-  a {
-    color: var(--color-text-body);
-  }
-
-  .project-list {
-    background: var(--sl-color-neutral-100);
-    padding: var(--spacing-sm);
-    line-height: var(--line-height);
-  }
-
-  .project-list__heading {
-    line-height: var(--line-height);
-    font-weight: var(--font-weight-bold);
-    font-size: var(--font-size-lg);
-  }
-
-  ul[role='list'].project-list__list {
-    display: flex;
-    flex-direction: column;
-    gap: var(--spacing-xs);
-    line-height: var(--line-height-sm);
-  }
-`;
+export const styles = isServer ? (await import('./index.css.js')).styles : undefined;
 
 if (!isServer) {
   await import('./index.js');
@@ -107,7 +25,7 @@ import './components/time-since/time-since.js';
 import type { SteamGameDetails } from '../functions/steam/SteamAPI.js';
 export default async () => {
   let games: SteamGameDetails[] = [];
-  let movies: any[] = [];
+  // let movies: any[] = [];
   if (isServer) {
     const { fetchSteamGames } = await import('../functions/steam/steamGames.js');
     games = await fetchSteamGames();
