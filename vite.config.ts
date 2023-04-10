d1fd01dea094faa421d2f86deec9f0f723faa18e
Willson Smith <me@willsonsmith.com>
2023-04-09 22:07:51 -0400

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import { glob } from 'glob';
 
 const htmlFiles = await glob('pyre/**/*.html');
-Object.fromEntries(htmlFiles.map((file) => [file, file]));
+console.log(Object.fromEntries(htmlFiles.map((file) => [file, file])));
 
 export default defineConfig({
   root: 'pyre',
