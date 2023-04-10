30ef4d109a0d19edf88b1a71e2cc888d1cdb0532
Willson Smith <me@willsonsmith.com>
2023-04-09 22:45:27 -0400

    should fix assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import topLevelAwait from 'vite-plugin-top-level-await';
 import { glob } from 'glob';
 
 const htmlFiles = await glob('pyre/**/*.html');
-console.log(Object.fromEntries(htmlFiles.map((file) => [file, file])));
 
 export default defineConfig({
   root: 'pyre',
-  publicDir: 'assets',
   plugins: [topLevelAwait()],
   build: {
     outDir: '../web',
