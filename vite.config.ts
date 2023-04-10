23aa74b590779ff569fec57fb3b81a31266fde5e
Willson Smith <me@willsonsmith.com>
2023-04-10 00:01:03 -0400

    better compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { glob } from 'glob';
 
 const htmlFiles = await glob('pyre/**/*.html');
 
 export default defineConfig({
   root: 'pyre',
-  plugins: [],
+  plugins: [minifyHTML.default()],
   build: {
     outDir: '../web',
     emptyOutDir: true,
     target: 'es2022',
+
     rollupOptions: {
       input: Object.fromEntries(htmlFiles.map((file) => [file, file])),
     },
